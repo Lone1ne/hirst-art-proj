e9bacf4d64fd26d73dab8213cb0bf836936b9e7a
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-02-20 18:23:25 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 20)
+#
+# # for color in colors:
+# #     rgb_colors.append(color.rgb)
+# #
+# # print(rgb_colors)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+import turtle as t
+import random
+t.colormode(255)
+tim = t.Turtle()
+
+color_list = [(198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5), (229, 159, 46), (27, 40, 157), (215, 74, 12), (15, 154, 16), (199, 14, 10), (243, 33, 165), (229, 17, 121), (73, 9, 31), (60, 14, 8), (224, 141, 211), (10, 97, 61)]
+tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+tim.speed(50)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = t.Screen()
+screen.exitonclick()
